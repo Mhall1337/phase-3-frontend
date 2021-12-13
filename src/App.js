5d335ea2c949b7d9f742e72982e359192c7b8410
Mitchell <Hmitchell.hall@gmail.com>
2021-12-13 13:49:04 -0600

    route for home component wasnt working so it was removed

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home';
 import React from 'react';
-import { Route } from 'react-router';
 
 function App() {
   return (
     <div className="App">
-      <Route exact path="/">
+      
         <Home />
-      </Route>
+      
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
