a6f5140134805504f3071c0414188619f9d8947d
Mitchell <Hmitchell.hall@gmail.com>
2021-12-16 15:57:02 -0600

    moved fetch to LaunchScreen component, and fixed an error where state was first returning undefined. also beginning to add conents to page

--- src/App.js
+++ src/App.js
@@ -4,17 +4,12 @@ import Home from './components/Home';
 import React, { useEffect, useState } from 'react';
 
 function App() {
-  const [launches, setLaunches] = useState()
-  useEffect(()=>{
-    fetch("http://localhost:9292/launches")
-    .then((r) => r.json())
-    .then(launches => setLaunches(launches))
-  },[])
+
 
   return (
     <div className="App">
         <Home />
-        <LaunchScreen launches ={launches}/>
+        <LaunchScreen />
     </div>
   );
 }
--- src/components/LaunchScreen.js
+++ src/components/LaunchScreen.js
@@ -1,9 +1,22 @@
+import { useState } from "react"
+import { useEffect } from "react"
+function LaunchScreen(){
+    const [launches, setLaunches] = useState([])
+    useEffect(()=>{
+      fetch("http://localhost:9292/launches")
+      .then((r) => r.json())
+      .then(launches => setLaunches(launches))
+    },[])
 
-function LaunchScreen({launches}){
-    console.log(launches[1].name)
+    console.log(launches[1])
 return(
     <div>
        launches
+       {launches.map(launch=>
+           <div>
+            <h4>Name:{launch.name}</h4>
+           </div> 
+       )}
     </div>
 )
 }
