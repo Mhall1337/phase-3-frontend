a8d3b9449fca5d03f82fce2e56f8a2f5a91acf83
Mitchell <Hmitchell.hall@gmail.com>
2021-12-16 16:03:39 -0600

    most launch data rendering except for flight success

--- src/components/LaunchScreen.js
+++ src/components/LaunchScreen.js
@@ -8,13 +8,16 @@ function LaunchScreen(){
       .then(launches => setLaunches(launches))
     },[])
 
-    console.log(launches[1])
+     console.log(launches[1])
 return(
     <div>
        launches
-       {launches.map(launch=>
-           <div>
+       {launches.map((launch, index)=>
+           <div key={index}>
             <h4>Name:{launch.name}</h4>
+            <p>Flight was successful:{launch.success}</p>
+            <p><strong>Outcome Details:</strong> {launch.details}</p>
+            <p>Flight number: {launch.flight_number}</p>
            </div> 
        )}
     </div>
