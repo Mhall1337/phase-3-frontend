595bac4e2b3c1574541b07f340858c9f302a54f2
Mitchell <Hmitchell.hall@gmail.com>
2021-12-16 14:51:57 -0600

    fetch is working and connected to db. added useState and added fetched launches to state via fetch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [launches, setLaunches] = useState()
   useEffect(()=>{
     fetch("http://localhost:9292/launches")
-    .then(r => r.json())
-    .then(launches => console.log(launches))
+    .then((r) => r.json())
+    .then(launches => setLaunches(launches))
   },[])
 
   return (
