c226bd66e7c2865a8ef18bc01e1d71e05d603a67
Mitchell <Hmitchell.hall@gmail.com>
2021-12-20 11:28:28 -0600

    changed the order of elements in newLaunch so it was added to db properly

--- src/components/CreateLaunch.js
+++ src/components/CreateLaunch.js
@@ -3,20 +3,20 @@ function CreateLaunch(){
         e.preventDefault()
         const newLaunch = {
             name : e.target[0].value,
-            details: e.target[1].value,
-            success: e.target[2].value,
+            details: e.target[2].value,
+            success: e.target[1].value,
         }
         console.log(newLaunch)
-    // fetch("http://localhost:9292/launches",{
-    //     method: "POST",
-    //     headers:{
-    //         "content-type" : "application/json",
-    //         "accept" : "application/json"
-    //     },
-    //     body: JSON.stringify()
-    // })
-    // .then(resp => resp.json())
-    // .then(resp => console.log(resp))
+    fetch("http://localhost:9292/launches",{
+        method: "POST",
+        headers:{
+            "content-type" : "application/json",
+            "accept" : "application/json"
+        },
+        body: JSON.stringify(newLaunch)
+    })
+    .then(resp => resp.json())
+    .then(resp => console.log(resp))
     }
     return(
         <div>
