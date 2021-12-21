2d36f03731c6adbfa20533fb97ab58996fa3413b
Mitchell <Hmitchell.hall@gmail.com>
2021-12-21 14:10:39 -0600

    added xml for CreateCompany. needs POST to db

---
+++ src/components/CreateCompany.js
@@ -0,0 +1,12 @@
+function CreateCompany(){
+    return(
+        <div>
+            <h1> Create A Company</h1>
+            <label> Company Name:
+                <input type="text" placeholder="Company Name"></input>
+            </label>
+            <button>Add</button>
+        </div>
+    )
+}
+export default CreateCompany
