397e5c9c94c2dd895ee719971807cea98e5d186a
Mitchell <Hmitchell.hall@gmail.com>
2021-12-21 14:42:42 -0600

    delete button works. it updates state and removes the company from the database

--- src/components/Companies.js
+++ src/components/Companies.js
@@ -13,13 +13,21 @@ function Companies(){
     function handleNewCompany(company){
         setCompanies([...allCompanies, company])
     }
+    function deleteCompany({id}){
+        setCompanies(allCompanies.filter(launch => launch.id !== id))
+        fetch(`http://localhost:9292/companies/${id}`,{
+            method: 'DELETE'
+        })
+            .then(resp => resp.json())
+            .then()
+    }
 
     return(
         <div>
             <CreateCompany handleNewCompany={handleNewCompany}/>
             {allCompanies.map((company, index) => 
-            <div>
-                <p key={index}>{company.company_name}<button>remove</button></p>
+            <div key={index}>
+                <p>{company.company_name}<button onClick={()=>deleteCompany(company)}>remove</button></p>
                 
             </div>
             )}
